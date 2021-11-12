9d04ea537eb89af67f4eedf44d335cddc453f64c
TamTH-Dev <tamth.dev@gmail.com>
2021-11-12 18:37:15 +0700

    Add SSG for projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,36 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import axios from 'axios'
 import { Container, SimpleGrid } from '@chakra-ui/react'
+import { GetStaticProps } from 'next'
 
 import ArticleLayout from '../components/layouts/article'
 import Section from '../components/Section'
 import { RepoGridItem } from '../components/GridItem'
 
-interface IRepo {
+interface IProject {
   id: number
   html_url: string
   name: string
   description: string
 }
 
-const Works = () => {
-  const [repos, setRepos] = useState([])
-  const reposUrl = 'https://api.github.com/users/TamTH-Dev/repos'
+interface IProjectsProps {
+  repos: Array<IProject>
+}
 
-  useEffect(() => {
-    const fetchRepos = async () => {
-      const res = await axios.get(reposUrl)
-      setRepos(res.data as [])
-    }
-
-    fetchRepos()
-  }, [])
-
+const Projects = ({ repos }: IProjectsProps) => {
   return (
     <ArticleLayout title="Personal Projects">
       <Container maxW="container.md">
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
-          {repos.map((repo: IRepo) => (
+          {repos.map(repo => (
             <Section key={repo.id}>
               <RepoGridItem
                 thumbnail="/images/github.png"
@@ -48,4 +41,16 @@ const Works = () => {
   )
 }
 
-export default Works
+export const getStaticProps: GetStaticProps = async () => {
+  const reposUrl = 'https://api.github.com/users/TamTH-Dev/repos'
+  const res = await axios.get(reposUrl)
+
+  return {
+    props: {
+      repos: res.data,
+    },
+    revalidate: 60 * 60 * 24, // Refresh data after 1 day
+  }
+}
+
+export default Projects
